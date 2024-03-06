9641374efbfb087314947774c1239f7ea0e5f3fd
UNKQULE <olikh04@gmail.com>
2024-03-06 23:33:17 +0400

    Ваше сообщение о коммите

--- app/src/main/java/com/example/words/StatisticsActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.words;
-
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class StatisticsActivity extends AppCompatActivity {
-}
